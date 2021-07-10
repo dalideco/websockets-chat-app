d0697df5a762e40f8b679bf348fca04d4c85ff36
dali <dalissj1@gmail.com>
2021-07-11 00:21:17 +0100

    fixing position

--- src/Text.js
+++ src/Text.js
@@ -9,6 +9,7 @@ import { useSelector } from "react-redux";
 export default function Text() {
 
     const fakeData = useSelector(state=> state.messages);
+    const {username } = useSelector(state=> state.inputs)
 
     
 
@@ -16,7 +17,7 @@ export default function Text() {
         <div className="text">
             {fakeData.map((data,index)=>(
                 <div className="message" key={index}>
-                    <div className={`cadre ${(data.sender==='dali')?'current-user': 'other-user'}`}>
+                    <div className={`cadre ${(data.sender===username)?'current-user': 'other-user'}`}>
                         {data.message + ' '} 
                         <p>{data.sender}</p>
                     </div>
